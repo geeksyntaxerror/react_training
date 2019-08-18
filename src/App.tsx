d179d2711cdc941afdc223a1ec0b7dfba32d74c3
kiran <kirankumar.entransys@gmail.com>
2019-08-18 18:36:18 +0530

    data sent from app component to main component via props

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,44 @@ import NavBar from './components/header/nav-bar';
 import SideBar from './components/sidebar/sidebar';
 import Main from './components/main_body/main';
 
+const data = [
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur'],
+  ['1,014','per','inceptos','himenaeos','Curabitur']
+];
+
 class App extends Component {
   render() {
     return (
@@ -12,7 +50,7 @@ class App extends Component {
         <div className="container-fluid">
           <div className="row">
             <SideBar />
-            <Main />
+            <Main data = { data }/>
           </div>
         </div>
       </div>
