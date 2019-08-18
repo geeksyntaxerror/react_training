3f60ba2bce0a3b21c8bb92968951cecf9e3122b1
kiran <kirankumar.entransys@gmail.com>
2019-08-18 07:41:59 +0530

    arrow function is used instead of class in nav-bar, main, sidebar components

--- src/components/header/nav-bar.tsx
+++ src/components/header/nav-bar.tsx
@@ -1,20 +1,33 @@
-import React , {Component} from 'react'
+import React, { Component } from 'react'
 import './nav-bar.css';
 
 
-class NavBar extends Component{
-    render(){
-        return (
-            <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-            <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-            <ul className="navbar-nav px-3">
-              <li className="nav-item text-nowrap">
-                <a className="nav-link" href="#">Sign out</a>
-              </li>
-            </ul>
-          </nav>
-        );
-    }
+// class NavBar extends Component{
+//     render(){
+//         return (
+//             <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+//             <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+//             <ul className="navbar-nav px-3">
+//               <li className="nav-item text-nowrap">
+//                 <a className="nav-link" href="#">Sign out</a>
+//               </li>
+//             </ul>
+//           </nav>
+//         );
+//     }
+// }
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+      <ul className="navbar-nav px-3">
+        <li className="nav-item text-nowrap">
+          <a className="nav-link" href="#">Sign out</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
