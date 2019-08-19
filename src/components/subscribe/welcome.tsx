90caa1c66c58d232215d27260addbc3f992f9024
kiran <kirankumar.entransys@gmail.com>
2019-08-19 18:53:24 +0530

    changeMessage method changed to arrow function

--- src/components/subscribe/welcome.tsx
+++ src/components/subscribe/welcome.tsx
@@ -1,44 +1,44 @@
 import React, { Component } from 'react'
 
-interface Props{
+interface Props {
 
 }
-interface MyState{
-    message : string;
-    button : string;
+interface MyState {
+    message: string;
+    button: string;
 }
 
-class Welcome extends Component<Props,MyState> {
-   constructor(props : Props){
-       super(props);
-       this.state = {
-           message : 'Welcome Visitor',
-           button : 'Subscribe'
-       }
-   }
+class Welcome extends Component<Props, MyState> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            message: 'Welcome Visitor',
+            button: 'Subscribe'
+        }
+    }
 
-    render(){
+    render() {
         return (
             <div>
                 <h1>{this.state.message}</h1>
-                <button onClick={()=>this.changeMessage()}>{this.state.button}</button>
+                <button onClick={this.changeMessage}>{this.state.button}</button>
             </div>
         );
     }
 
-    changeMessage(){
-        if(this.state.message == 'Welcome Visitor'){
+    changeMessage = () => {
+        if (this.state.message == 'Welcome Visitor') {
             this.setState({
-                message : 'Thanks for Subscribing',
-                button : 'Subscribed'
+                message: 'Thanks for Subscribing',
+                button: 'Subscribed'
             });
         }
-        else{
+        else {
             this.setState({
-                message : 'Already Subscribed'
+                message: 'Already Subscribed'
             });
         }
-        
+
     }
 }
 
